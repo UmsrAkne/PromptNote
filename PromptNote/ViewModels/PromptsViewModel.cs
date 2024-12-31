5507574559a91bdffd53613cb0314817af612910
Akn <sibata486@gmail.com>
2024-12-31 20:23:48 +0900

    add feat / リストアイテムから、入力可能なプロンプトのテキストを生成する機能を実装。

--- PromptNote/ViewModels/PromptsViewModel.cs
+++ PromptNote/ViewModels/PromptsViewModel.cs
@@ -1,4 +1,7 @@
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Prism.Commands;
 using Prism.Mvvm;
 using PromptNote.Models;
 
@@ -8,5 +11,14 @@ namespace PromptNote.ViewModels
     public class PromptsViewModel : BindableBase
     {
         public ObservableCollection<Prompt> Prompts { get; set; } = new ();
+
+        /// <summary>
+        /// このビューモデルが持つ Prompts から、実際に入力可能な形式のテキストを生成してクリップボードにコピーします。
+        /// </summary>
+        public DelegateCommand GeneratePromptCommand => new DelegateCommand(() =>
+        {
+            var prs = Prompts.Select(p => p.Phrase);
+            Clipboard.SetText(string.Join(',', prs).Replace(",", ", "));
+        });
     }
 }
