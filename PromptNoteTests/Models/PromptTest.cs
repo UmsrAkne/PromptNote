50c896f3c2f8091da64c939dda1ea16f379f492a
Akn <sibata486@gmail.com>
2025-01-13 15:16:07 +0900

    add test / Prompt の出力に関するテストを追加。

---
+++ PromptNoteTests/Models/PromptTest.cs
@@ -0,0 +1,17 @@
+using PromptNote.Models;
+
+namespace PromptNoteTests.Models
+{
+    [TestFixture]
+    public class PromptTest
+    {
+        [Test]
+        [TestCase("test", PromptType.Normal, "test, ")]
+        [TestCase("<lora:test>", PromptType.Lora, "<lora:test:1>, ")]
+        public void ToStringTest(string phrase, PromptType type, string expected)
+        {
+            var actual = new Prompt() { Phrase = new Phrase() { Value = phrase, }, Type = type, }.ToString();
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
