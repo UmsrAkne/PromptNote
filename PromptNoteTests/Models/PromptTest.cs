654d96fc2dea3dcf1c900acad083ce289000e8bf
Akn <sibata486@gmail.com>
2025-01-14 18:38:24 +0900

    update feat / Prompt.ToString() の仕様を変更した。
    Prompt.Phrase の文字列にカンマをつけて出力していた。
    リストから出力することを考えての実装だったが、Prompt クラス単体がこのような処理をするのは余計だと感じたため、単純に Phrase の内容を出力するよう仕様変更した。

--- PromptNoteTests/Models/PromptTest.cs
+++ PromptNoteTests/Models/PromptTest.cs
@@ -6,8 +6,8 @@ namespace PromptNoteTests.Models
     public class PromptTest
     {
         [Test]
-        [TestCase("test", PromptType.Normal, "test, ")]
-        [TestCase("<lora:test>", PromptType.Lora, "<lora:test:1>, ")]
+        [TestCase("test", PromptType.Normal, "test")]
+        [TestCase("<lora:test>", PromptType.Lora, "<lora:test:1>")]
         public void ToStringTest(string phrase, PromptType type, string expected)
         {
             var actual = new Prompt() { Phrase = new Phrase() { Value = phrase, }, Type = type, }.ToString();
