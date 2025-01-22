edfffc195c5be148dff8a3f777c42c034b069248
Akn <sibata486@gmail.com>
2025-01-22 15:57:08 +0900

    add class / TagMap.cs

---
+++ PromptNote/Models/TagMap.cs
@@ -0,0 +1,11 @@
+namespace PromptNote.Models
+{
+    public class TagMap
+    {
+        public int Id { get; set; }
+
+        public int TagId { get; set; }
+
+        public int PhraseId { get; set; }
+    }
+}
