bc6b9c3011a43f1483bacfaf92cb9cd0c5660e86
Akn <sibata486@gmail.com>
2025-02-03 16:06:13 +0900

    update feat / プロンプトの追加時、グループIDを設定する処理を追加。

--- PromptNote/ViewModels/MainWindowViewModel.cs
+++ PromptNote/ViewModels/MainWindowViewModel.cs
@@ -53,6 +53,11 @@ namespace PromptNote.ViewModels
 
             if (p != null)
             {
+                if (PromptGroupViewModel.SelectedItem != null)
+                {
+                    p.GroupId = PromptGroupViewModel.SelectedItem.Id;
+                }
+
                 await PromptsViewModel.AddItemAsync(p);
             }
 
@@ -157,7 +162,7 @@ namespace PromptNote.ViewModels
 
             PromptsViewModel.ReIndex();
 
-            PromptGroupViewModel.PromptGroups.Add(new PromptGroup() { Name = "Test Group1", });
+            PromptGroupViewModel.PromptGroups.Add(new PromptGroup() { Name = "Test Group1", Id = 2, });
             PromptGroupViewModel.PromptGroups.Add(
                 new PromptGroup()
                 {
@@ -184,6 +189,8 @@ namespace PromptNote.ViewModels
             PromptGroupViewModel.PromptGroups.Add(new PromptGroup() { Name = "Test Group3", });
 
             await PromptGroupViewModel.LoadGroupsAsyncCommand.ExecuteAsync();
+
+            PromptGroupViewModel.SelectedItem = PromptGroupViewModel.PromptGroups[0];
 
             // リストを初期化
             PromptsViewModel.SetItems(new ObservableCollection<Prompt>());
